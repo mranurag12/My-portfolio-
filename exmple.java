5f77d04a3ad82f78da346d26e628d5c31f1be97d
Anurag Mishra <164143171+mranurag12@users.noreply.github.com>
2024-03-20 16:58:40 +0530

    added images

---
+++ exmple.java
@@ -0,0 +1,10 @@
+public class exemplie{
+    public static void main (string []args)
+     int a = 93 ;
+     if (a%2==0){
+        sytem.out.println(x:"Even");
+     }
+     else{
+        sytem.out.println(x:"odd");
+     }
+}
